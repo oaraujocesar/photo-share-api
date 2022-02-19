82ed0707ea406a6445552baf9309194956554c42
César O. Araújo <oaraujocesar@gmail.com>
2022-02-19 18:25:03 -0300

    feat: Post a new photo and resolving the url

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `;
 
+let _id = 0;
 let photos = [];
 
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
+    allPhotos: () => photos,
   },
 
   Mutation: {
-    postPhoto: (_, photo) => photos.push(photo),
+    postPhoto: (_, photo) => {
+      let newPhoto = {
+        id: _id++,
+        ...photo,
+      };
+
+      photos.push(newPhoto);
+
+      return photos[photos.length - 1];
+    },
+  },
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
   },
 };
 
